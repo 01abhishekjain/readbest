e3906e715f35bc853b61b2beedd279dcd86df107
Abhishek Jain (MBP) <ajain@bluestacks.com>
2022-07-27 02:03:11 +0530

    playing

--- readbest/src/components/hero.jsx
+++ readbest/src/components/hero.jsx
@@ -3,20 +3,17 @@ function Hero() {
 		<div className="hero">
 			<div className="hero__container">
 				<div className="hero__domain" title="Click to see a sample">
-					<span className="hero__domain--fake"></span>
-					<span className="hero__domain__actual--container">
-						<a
-							href="https://read.best/https://n.pr/3J4EpSn"
-							target="_blank"
-							rel="noreferrer"
-							className="hero__domain__actual"
-						>
-							<span className="hero__domain__actual--host">read.best</span>
-							<span className="hero__domain__actual--path">
-								/https://n.pr/3J4EpSn
-							</span>
-						</a>
-					</span>
+					<a
+						href="https://read.best/https://n.pr/3J4EpSn"
+						target="_blank"
+						rel="noreferrer"
+						className="hero__domain__actual"
+					>
+						<span className="hero__domain__actual--host">read.best</span>
+						<span className="hero__domain__actual--path">
+							/https://n.pr/3J4EpSn
+						</span>
+					</a>
 				</div>
 				<div className="hero__title">
 					The <span>best</span> way to <span>read</span> online <span>!</span>
@@ -25,15 +22,11 @@ function Hero() {
 					Simply{' '}
 					<span
 						onMouseOver={function () {
-							const elem = document.querySelector(
-								'.hero__domain__actual--container'
-							);
+							const elem = document.querySelector('.hero__domain__actual');
 							elem?.classList.add('highlight');
 						}}
 						onMouseOut={function () {
-							const elem = document.querySelector(
-								'.hero__domain__actual--container'
-							);
+							const elem = document.querySelector('.hero__domain__actual');
 							elem?.classList.remove('highlight');
 						}}
 						title="Try clicking the link above"
